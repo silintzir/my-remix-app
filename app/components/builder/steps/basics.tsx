8571e6412cc95907512f96d9991d91610cfde2a9
Panayotis Silintziris <psilintziris@protonmail.com>
2024-01-16 13:10:48 +0200

    fix the google maps address

--- app/components/builder/steps/basics.tsx
+++ app/components/builder/steps/basics.tsx
@@ -1,9 +1,34 @@
 import { TextInput } from "@/components/shadcn/TextInput";
 import type { ResumeValues } from "@/lib/types";
 import { useFormContext } from "react-hook-form";
+import { usePlacesWidget } from "react-google-autocomplete";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 export function BasicsStep() {
-  const { control } = useFormContext<ResumeValues>();
+  const { control, setValue, getValues } = useFormContext<ResumeValues>();
+
+  const {
+    resume: {
+      basics: {
+        location: { address },
+      },
+    },
+  } = getValues();
+
+  const { ref: maps } = usePlacesWidget({
+    apiKey: "AIzaSyB7-fKIMmJ7jAA_rvlz7KpR6h_4GBshRIc",
+    options: {
+      fields: ["ALL"],
+      types: ["geocode"],
+      componentRestrictions: { country: "us" },
+    },
+    onPlaceSelected: (place) => {
+      setValue("resume.basics.location.address", place.formatted_address, {
+        shouldDirty: true,
+      });
+    },
+  });
 
   return (
     <div className="space-y-2">
@@ -38,12 +63,16 @@ export function BasicsStep() {
         />
       </div>
       <div className="flex gap-2 sm:flex-8 flex-wrap sm:flex-nowrap">
-        <TextInput
-          control={control}
-          name="resume.basics.location.address"
-          placeholder="e.x. State 100 Street, Boston 01978, MA"
-          label="Address"
-        />
+        <div className="w-full space-y-2">
+          <Label>Address</Label>
+          <Input
+            ref={maps as any}
+            defaultValue={address}
+            name="resume.basics.location.address"
+            placeholder="e.x. State 100 Street, Boston 01978, MA"
+            className="w-full"
+          />
+        </div>
         <TextInput
           control={control}
           name="resume.basics.url"
