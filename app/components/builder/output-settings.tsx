c852b0b86f632b28f1ea00e718b41fb9e418397b
Panayotis Silintziris <psilintziris@protonmail.com>
2024-03-15 13:47:28 +0200

    move template dropdown outside advanced settings

--- app/components/builder/output-settings.tsx
+++ app/components/builder/output-settings.tsx
@@ -1,12 +1,12 @@
-import { Button } from "@/components/ui/button";
-import { Label } from "@/components/ui/label";
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover";
-import { SlidersHorizontal } from "lucide-react";
-import { FontSizeAdjust } from ".";
+// import { Button } from "@/components/ui/button";
+// import { Label } from "@/components/ui/label";
+// import {
+//   Popover,
+//   PopoverContent,
+//   PopoverTrigger,
+// } from "@/components/ui/popover";
+// import { SlidersHorizontal } from "lucide-react";
+// import { FontSizeAdjust } from ".";
 import { useFormContext } from "react-hook-form";
 import type { ResumeValues } from "@/lib/types";
 import { SelectInput } from "../shadcn/SelectInput";
@@ -39,51 +39,57 @@ export function OutputSettings({ values }: Props) {
   }, [open, setOpen, getValues, translator]);
 
   return (
-    <Popover>
-      <PopoverTrigger asChild>
-        <Button variant="ghost">
-          <SlidersHorizontal />
-          <span>Settings</span>
-        </Button>
-      </PopoverTrigger>
-      <PopoverContent className="w-80 space-y-4">
-        <div className="flex items-center gap-4">
-          <Label htmlFor="maxWidth">Language</Label>
-          <SelectInput
-            className="w-full"
-            control={control}
-            options={[
-              { label: "English", value: "en" },
-              { label: "Spanish", value: "es" },
-            ]}
-            onChange={(value) => {
-              setTimeout(() => {
-                setOpen(value);
-              }, 1000);
-            }}
-            name="meta.language"
-          />
-        </div>
-        <div className="grid grid-cols-3 items-center gap-4">
-          <Label htmlFor="width">Font size</Label>
-          <FontSizeAdjust />
-          <strong>{values.meta.fontSize}pt</strong>
-        </div>
-        <div className="grid grid-cols-3 items-center gap-4">
-          <Label htmlFor="width">Template</Label>
-          <SelectInput
-            className="w-full"
-            control={control}
-            options={[
-              { label: "Chicago", value: "chicago" },
-              { label: "Admin executive", value: "executive" },
-              { label: "Accountant WIP", value: "accountant" },
-            ]}
-            name="meta.template"
-          />
-          <strong>{values.meta.fontSize}pt</strong>
-        </div>
-      </PopoverContent>
-    </Popover>
+    <div className="flex gap-2 items-center">
+      <label>Template: </label>
+      <SelectInput
+        className="w-full text-black"
+        control={control}
+        options={[
+          { label: "Chicago", value: "chicago" },
+          { label: "Admin executive", value: "executive" },
+          { label: "Accountant WIP", value: "accountant" },
+        ]}
+        name="meta.template"
+      />
+    </div>
   );
+
+  // return (
+  //   <Popover>
+  //     <PopoverTrigger asChild>
+  //       <Button variant="ghost">
+  //         <SlidersHorizontal />
+  //         <span>Settings</span>
+  //       </Button>
+  //     </PopoverTrigger>
+  //     <PopoverContent className="w-80 space-y-4">
+  //       <div className="flex items-center gap-4">
+  //         <Label htmlFor="maxWidth">Language</Label>
+  //         <SelectInput
+  //           className="w-full"
+  //           control={control}
+  //           options={[
+  //             { label: "English", value: "en" },
+  //             { label: "Spanish", value: "es" },
+  //           ]}
+  //           onChange={(value) => {
+  //             setTimeout(() => {
+  //               setOpen(value);
+  //             }, 1000);
+  //           }}
+  //           name="meta.language"
+  //         />
+  //       </div>
+  //       <div className="grid grid-cols-3 items-center gap-4">
+  //         <Label htmlFor="width">Font size</Label>
+  //         <FontSizeAdjust />
+  //         <strong>{values.meta.fontSize}pt</strong>
+  //       </div>
+  //       <div className="grid grid-cols-3 items-center gap-4">
+  //         <Label htmlFor="width">Template</Label>
+  //         <strong>{values.meta.fontSize}pt</strong>
+  //       </div>
+  //     </PopoverContent>
+  // //   </Popover>
+  // );
 }
