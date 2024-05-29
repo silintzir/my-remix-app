00d3aa01ebff23c450f5a96b86a55805e9bea2f9
Panayotis Silintziris <psilintziris@gmail.com>
2024-05-29 16:49:56 +0300

    remove translation popup from flag icon

--- app/components/builder/translate-modal.tsx
+++ app/components/builder/translate-modal.tsx
@@ -1,18 +1,7 @@
-import { Button } from "@/components/ui/button";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-} from "@/components/ui/dialog";
 import type { ResumeValues } from "@/lib/types";
 import { useFormContext } from "react-hook-form";
 import usFlag from "@/images/us-flag.svg";
 import esFlag from "@/images/es-flag.svg";
-import { useFetcher } from "@remix-run/react";
-import { useEffect, useState } from "react";
 
 type Props = {
   resumeId: number;
@@ -21,68 +10,16 @@ type Props = {
 export function TranslateModal({ resumeId }: Props) {
   const { watch } = useFormContext<ResumeValues>();
 
-  const [open, setOpen] = useState(false);
   const lang = watch("meta.language");
-  const target = lang === "en" ? "Spanish" : "English";
-  const { submit, state } = useFetcher({ key: "translator" });
-
-  useEffect(() => {
-    if (state === "loading") {
-      setOpen(false);
-    }
-  }, [state]);
 
   return (
-    <Dialog open={open}>
-      <button onClick={() => setOpen(true)}>
-        <span>
-          <img
-            src={lang === "en" ? usFlag : esFlag}
-            height="16"
-            width="16"
-            alt="Language"
-          />
-        </span>
-      </button>
-      <DialogContent className="sm:max-w-md">
-        <DialogHeader>
-          <DialogTitle>Translate</DialogTitle>
-          <DialogDescription>
-            You may translate your resume to {target}
-          </DialogDescription>
-        </DialogHeader>
-        <DialogFooter className="sm:justify-start">
-          <Button
-            type="button"
-            variant="default"
-            disabled={state === "submitting"}
-            onClick={() => {
-              const fd = new FormData();
-              fd.append("id", resumeId.toString());
-              fd.append("target", target);
-              submit(fd, { action: `/ai/translate`, method: "POST" });
-            }}
-          >
-            <img
-              src={lang === "en" ? esFlag : usFlag}
-              height="16"
-              width="16"
-              alt="Language"
-              className="mr-2"
-            />
-            {state === "submitting"
-              ? "Translating...This may take a while."
-              : `Translate to ${target}`}
-          </Button>
-          <Button
-            type="button"
-            variant="outline"
-            onClick={() => setOpen(false)}
-          >
-            Close
-          </Button>
-        </DialogFooter>
-      </DialogContent>
-    </Dialog>
+    <span>
+      <img
+        src={lang === "en" ? usFlag : esFlag}
+        height="16"
+        width="16"
+        alt="Language"
+      />
+    </span>
   );
 }
